4bdc2567ed93097de75277bb948bd691b24add3e
Rene Avila <reneavila1993@gmail.com>
2020-06-23 23:04:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ function unleashDog(dogName, dogBreed) {
   return sentence;
 }
 
-let routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()]
+var routine = [wakeDog(), leashDog(), walkToPark(), throwFrisbee(), walkHome(), unleashDog()]
